423a263867f97cccd5a2d6e61ebc10d4f5356447
nguboRandy <nguborandy@gmail.com>
2024-06-11 22:15:31 +0200

    Add project files.

---
+++ RunDepartment/Department.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunDepartment
+{
+    internal class Department
+    {
+
+        public double[][] sales;
+
+        public Department()
+        {
+            sales = new double[4][];
+            for (int i = 0; i < 4; i++)
+            {
+                sales[i] = new double[3];
+            }
+        }
+
+        public void PopulateArray()
+        {
+            Console.WriteLine("Enter sales figures for each department and month:");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Department {i + 1}:");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Month {j + 1}: ");
+                    sales[i][j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+        }
+
+        public double[] CalculateAvgSalesPerMonth()
+        {
+            double[] avgSales = new double[3];
+
+            for (int j = 0; j < 3; j++)
+            {
+                double total = 0;
+                int count = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    total += sales[i][j];
+                    if (sales[i][j] > 0)
+                        count++;
+                }
+                avgSales[j] = total / count;
+            }
+
+            return avgSales;
+        }
+
+        public double[] CalculateAvgSalesPerDepart()
+        {
+            double[] avgSales = new double[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                double total = 0;
+                int count = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    total += sales[i][j];
+                    if (sales[i][j] > 0)
+                        count++;
+                }
+                avgSales[i] = total / count;
+            }
+
+            return avgSales;
+        }
+
+        public int DetermineHighestMonthlySales(int month)
+        {
+            int highestDepartment = -1;
+            double highestSales = -1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (sales[i][month - 1] > highestSales)
+                {
+                    highestSales = sales[i][month - 1];
+                    highestDepartment = i + 1;
+                }
+            }
+
+            return highestDepartment;
+        }
+
+        public int DetermineHighestDepartSales(int department)
+        {
+            int highestMonth = -1;
+            double highestSales = -1;
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (sales[department - 1][j] > highestSales)
+                {
+                    highestSales = sales[department - 1][j];
+                    highestMonth = j + 1;
+                }
+            }
+
+            return highestMonth;
+        }
+    }
+}
---
+++ RunDepartment/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunDepartment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Department department = new Department();
+            department.PopulateArray();
+
+            double[] avgSalesPerMonth = department.CalculateAvgSalesPerMonth();
+            double[] avgSalesPerDepart = department.CalculateAvgSalesPerDepart();
+
+            Console.WriteLine("\nSales Figures per Department per Month:");
+            Console.WriteLine("                                                                 ");
+            Console.WriteLine("| Department    |    Month 1    |    Month 2    |    Month 3    |");
+            Console.WriteLine("|---------------|---------------|---------------|---------------|");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"| {i + 1}          | {department.sales[i][0],-8} | {department.sales[i][1],-8} | {department.sales[i][2],-8} |");
+            }
+
+
+
+            Console.WriteLine("\nAverage Sales per Department:");
+            for (int i = 0; i < avgSalesPerDepart.Length; i++)
+            {
+                Console.WriteLine($"Department {i + 1}: {avgSalesPerDepart[i]}");
+            }
+
+
+
+            Console.WriteLine("\nAverage Sales per Month:");
+            for (int i = 0; i < avgSalesPerMonth.Length; i++)
+            {
+                Console.WriteLine($"Month {i + 1}: {avgSalesPerMonth[i]}");
+            }
+
+            
+
+            Console.WriteLine("\nMonth with the Highest Sales per Department:");
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine($"Department {i}: Month {department.DetermineHighestDepartSales(i)}");
+            }
+
+            Console.WriteLine("\nDepartment with the Highest Sales per Month:");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Month {i}: Department {department.DetermineHighestMonthlySales(i)}");
+            }
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+
+}
+
